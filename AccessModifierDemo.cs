1c971b0b8d02ab150ed354127763220eb0e601db
Snehasrg <sghongade642@gmail.com>
2023-06-15 21:19:34 +0530

    [sneha]Added Program for AccessModifier example

---
+++ AccessModifierDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class AccessModifierDemo
+    {
+        public int id = 1;
+        private string name = "Raj";
+        internal int RollNumber = 7;
+        protected string username = "Raj@";
+    }
+    public class Employee : AccessModifierDemo//inherited class for protected access modifier
+    {
+        public void Print()
+        {
+            Console.WriteLine(username);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace Demo
         public static void Main(String[] args)
         {
             Console.WriteLine("Enter your option from below :-" +"\n1.Constructor Example." +
-                                "\n2.Interface Example.");
-          int Option=Convert.ToInt32( Console.ReadLine());
+                                "\n2.Interface Example." + "\n3.AccessModifier Example.");
+            int Option=Convert.ToInt32( Console.ReadLine());
 
             switch(Option)
             {
@@ -21,6 +21,17 @@ namespace Demo
                     Dog dog = new Dog();
                     dog.animalSound();
                     break; 
+                    case 3:
+                    AccessModifierDemo person = new AccessModifierDemo();
+                    Console.WriteLine(person.id);//created object for public class
+                 // Console.WriteLine(person.name);//created object for private class
+                    Console.WriteLine(person.RollNumber);//created object for internal class
+
+                  //Console.WriteLine(person.username);//created object for protected class
+                    Employee employee = new Employee();
+                    employee.Print();
+
+                    break;
             }
 
         }
