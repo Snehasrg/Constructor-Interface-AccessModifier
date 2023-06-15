b549d2cf29732ae6f713bf879046cf2f0db69ff1
Snehasrg <sghongade642@gmail.com>
2023-06-15 21:04:18 +0530

    [sneha]Added Program for constructor example

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Demo
+{
+    public class program
+    {
+        public static void Main(String[] args)
+        {
+
+            ConstructorDemo obj = new ConstructorDemo(10, "Raj");
+            Console.WriteLine(obj.age);
+            Console.WriteLine(obj.name);
+
+        }
+    }
+}
