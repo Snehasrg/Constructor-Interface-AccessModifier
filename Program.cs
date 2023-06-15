e367412f538af8e794a67ede4912826f75bc4845
Snehasrg <sghongade642@gmail.com>
2023-06-15 21:13:57 +0530

    [sneha]Added Program for interface example

--- Program.cs
+++ Program.cs
@@ -6,10 +6,22 @@ namespace Demo
     {
         public static void Main(String[] args)
         {
+            Console.WriteLine("Enter your option from below :-" +"\n1.Constructor Example." +
+                                "\n2.Interface Example.");
+          int Option=Convert.ToInt32( Console.ReadLine());
 
-            ConstructorDemo obj = new ConstructorDemo(10, "Raj");
-            Console.WriteLine(obj.age);
-            Console.WriteLine(obj.name);
+            switch(Option)
+            {
+                case 1:
+                    ConstructorDemo obj = new ConstructorDemo(10, "Raj");
+                    Console.WriteLine(obj.age);
+                    Console.WriteLine(obj.name);
+                    break;
+                case 2:
+                    Dog dog = new Dog();
+                    dog.animalSound();
+                    break; 
+            }
 
         }
     }
